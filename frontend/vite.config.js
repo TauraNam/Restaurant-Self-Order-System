57f29552f4e7072a51dedad40b83c3fc698f1a45
TauraNam <tauranamirskaite@gmail.com>
2025-02-09 16:26:49 +0200

    new packages introduced

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
+import dotenv from 'dotenv'
 import react from '@vitejs/plugin-react'
+dotenv.config()
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api')
       }
