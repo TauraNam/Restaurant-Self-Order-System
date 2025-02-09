cee99e0cbc733cea183bf199b8fc5093152c7888
TauraNam <tauranamirskaite@gmail.com>
2025-02-09 16:27:59 +0200

    Cart component changes

--- frontend/src/components/Restaurant/Cart.jsx
+++ frontend/src/components/Restaurant/Cart.jsx
@@ -1,47 +1,71 @@
-import CartCard from "./CartCard";
+import { useRef } from 'react'
+import CartCard from "./CartCard"
+import { AiOutlineClose } from "react-icons/ai"
+import { useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
 
 const Cart = ({ isCartOpen, toggleCart, cart, emptyCart, clearCartProduct }) => {
 
+    const navigate = useNavigate()
+    const takeAwayRef = useRef(null)
+    const notesRef = useRef(null)
+
     const calculateTotalAmount = () => {
-        if (!cart || cart.length === 0) return 0;
+        if (!cart || cart.length === 0) return 0
 
         return cart.reduce((total, item) => {
-            return total + (item.price * item.quantity);
-        }, 0);
-    };
+            return total + (item.price * item.quantity)
+        }, 0)
+    }
 
-    const totalAmount = calculateTotalAmount();
+    const totalAmount = calculateTotalAmount()
 
     const placeOrder = () => {
+        const takeAwayValue = takeAwayRef.current ? takeAwayRef.current.checked : false
+        const notesValue = notesRef.current ? notesRef.current.value : ''
+
+        const tableNumber = localStorage.getItem('tableNumber') + (takeAwayValue ? ' - Take Away' : '')
         const order = {
-            tableNumber: 12,
-            products: cart.map((product) => {
-                return {
-                    "product": product._id,
-                    "quantity": product.quantity
-                }
-            }),
+            tableNumber: tableNumber,
+            products: cart.map((product) => ({
+                product: product._id,
+                quantity: product.quantity
+            })),
             orderPrice: totalAmount,
-            notes: document.querySelector('#notes').value,
+            notes: notesValue,
             status: "Pending"
         }
-
 
         fetch('/api/orders', {
             method: 'POST',
             body: JSON.stringify(order),
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             }
         })
-        .then(response => {
-            if (!response.ok) throw new Error('Network response')
-        })
-        .catch(err => console.log('Error during fetch', err))
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Failed to place order')
+                }
+                return response.json()
+            })
+            .then((data) => {
+                navigate('/orders/' + data._id)
+                toast.success('Order successful!')
+                emptyCart()
+            })
+            .catch((err) => {
+                console.error('Error placing order:', err)
+                toast.warning('Something went wrong. Please try again later.')
+            })
     }
 
     return (
         <div className={`cart ${isCartOpen ? 'open' : ''}`}>
+            <div className="cart-close-button" onClick={toggleCart}>
+                <AiOutlineClose className="cart-close-icon" />
+            </div>
+
             <div className="cart-content">
                 <h2>My Order</h2>
                 {cart.length === 0 ? (
@@ -51,25 +75,34 @@ const Cart = ({ isCartOpen, toggleCart, cart, emptyCart, clearCartProduct }) => 
                         const button = (
                             <button className="cart-clear-item-button"
                                 onClick={() => clearCartProduct(item._id)}>
-                                    x
+                                <AiOutlineClose className="cart-icon" />
                             </button>
-                        );
-                    
+                        )
+
                         return (
                             <CartCard key={index} item={item} button={button} />
-                        );
+                        )
                     })
                 )}
                 <div className="cart-buttons">
                     <button className="empty-cart-button" onClick={emptyCart}>Empty Cart</button>
-                    <button className="close-cart-button" onClick={toggleCart}>Close Cart</button>
+                    <div className="cart-total-amount">
+                        <p>Total Amount: </p>
+                        <p>{totalAmount.toFixed(2)}€ </p>
+                    </div>
+                    <div className="cart-take-away">
+                        <label htmlFor="take-away">Take away?</label>
+                        <input type="checkbox" id="take-away" ref={takeAwayRef}></input>
+                    </div>
+                    <textarea
+                        ref={notesRef} // Priskiriame ref prie 'textarea'
+                        placeholder="Add your notes (optional)"
+                        rows="4"
+                        cols="40"
+                        className="notes"
+                    />
+                    <button className="checkout-cart-button" onClick={placeOrder}>Place Order</button>
                 </div>
-                <div className="cart-total-amount">
-                    <p>Total Amount: </p>
-                    <p>{totalAmount.toFixed(2)} € </p>
-                </div>
-                <textarea id="notes" placeholder="Add your notes (optional)" rows="4" cols="40" />
-                <button className="checkout-cart-button" onClick={placeOrder}>Place Order</button>
             </div>
         </div>
     );
