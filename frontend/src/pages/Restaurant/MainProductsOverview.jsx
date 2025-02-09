cb612bcbc922cf3ba95e2bada8fa38e17efba218
TauraNam <tauranamirskaite@gmail.com>
2025-02-09 16:28:32 +0200

    MainProductsOverview changes

--- frontend/src/pages/Restaurant/MainProductsOverview.jsx
+++ frontend/src/pages/Restaurant/MainProductsOverview.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
 import RestaurantHeader from "../../components/Restaurant/RestaurantHeader.jsx"
-import './Restaurant.css'
 import Cart from "../../components/Restaurant/Cart.jsx"
 import CategoriesList from "../../components/Restaurant/CategoriesList.jsx"
-
+import { AiOutlinePlus } from "react-icons/ai"
+import Pagination from "../../components/Shared/Pagination.jsx"
+import { useSearch } from "../../context/SearchContext.jsx"
 
 const MainProductsOverview = () => {
 
@@ -11,109 +12,107 @@ const MainProductsOverview = () => {
     const [products, setProducts] = useState([])
     const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart')) || [])
     const [activeCategory, setActiveCategory] = useState("All")
-    const [filteredProducts, setFilteredProducts] = useState([])
+    const [totalPages, setTotalPages] = useState(1)
+    const [currentPage, setCurrentPage] = useState(1)
+    const { searchText, setSearchText } = useSearch()
 
     const toggleCart = () => {
         setIsCartOpen(!isCartOpen)
     }
 
+    useEffect(() => {
+        setSearchText('')
+    }, [activeCategory])
 
     const getProducts = () => {
-        fetch('/api/products')
+        fetch(`/api/products?page=${currentPage}&limit=12${activeCategory !== "All" ? '&category=' + activeCategory._id : ""}`)
             .then(response => {
                 if (!response.ok) throw new Error('Network response.')
                 return response.json()
             })
-            .then(products => {
+            .then(({ products, totalPages }) => {
                 setProducts(products)
+                setTotalPages(totalPages)
             })
             .catch(err => console.log('Error during fetch.', err))
     }
 
+    const filteredProducts = products.filter(product =>
+        product.title.toLowerCase().includes(searchText.toLowerCase()) ||
+        product.description.toLowerCase().includes(searchText.toLowerCase()) ||
+        String(product.price).includes(searchText)
+    )
+
     useEffect(() => {
         getProducts()
-    }, [])
+    }, [currentPage, activeCategory])
 
-
-    const filterProductsByActiveCategory = () => {
-        if (activeCategory === 'All') {
-            setFilteredProducts(products)
-        } else {
-            setFilteredProducts(products.filter(item => item.category === activeCategory._id))
-        }  
+    const handlePageChange = (page) => {
+        if (page >= 1 && page <= totalPages) {
+            setCurrentPage(page)
+            window.scrollTo(0, 0)
+        }
     }
-
-    useEffect(() => {
-        filterProductsByActiveCategory()
-    }, [products, activeCategory])
-
 
     const addToCart = (product) => {
         try {
             let cartData = JSON.parse(localStorage.getItem('cart')) || []
 
             const existingProductIndex = cartData.findIndex(item => item._id === product._id)
-        
+
             if (existingProductIndex > -1) {
-                cartData[existingProductIndex].quantity += 1;
+                cartData[existingProductIndex].quantity += 1
             } else {
-                product.quantity = 1;
-                cartData.push(product)
+                const newProduct = { ...product, quantity: 1 }
+                cartData.push(newProduct)
             }
 
             localStorage.setItem('cart', JSON.stringify(cartData))
-
             setCart(cartData)
+
         } catch (err) {
-            console.log('Error during fetch', err)
+            console.error('Error during addToCart', err)
+            alert('Something went wrong when adding to cart')
         }
     }
 
     const clearCartProduct = (productId) => {
-        try {
-            let cartData = JSON.parse(localStorage.getItem('cart')) || []
-    
-            const updatedCartData = cartData.filter(item => item._id !== productId)
-    
-            localStorage.setItem('cart', JSON.stringify(updatedCartData))
-    
-            setCart(updatedCartData)
-        } catch (err) {
-            console.log('Error while clearing the product from cart:', err)
-        }
-    };
+        const updatedCart = cart.filter(item => item._id !== productId)
+        setCart(updatedCart)
+        localStorage.setItem('cart', JSON.stringify(updatedCart))
+    }
 
     const emptyCart = () => {
-        try {
-        localStorage.removeItem('cart')
         setCart([])
-    } catch (err) {
-        console.log('Error while emptying the cart', err)
+        localStorage.removeItem('cart')
     }
-}
 
     return (
         <div>
-            <RestaurantHeader toggleCart={toggleCart} />
-            <CategoriesList activeCategory={activeCategory} setActiveCategory={setActiveCategory} />
+            <RestaurantHeader toggleCart={toggleCart} cart={cart} />
+            <CategoriesList activeCategory={activeCategory} setActiveCategory={setActiveCategory} setCurrentPage={setCurrentPage} />
             <div className="products-overview">
-                <h2>Products</h2>
                 <div className="products-container">
                     {filteredProducts && filteredProducts.map((product, index) => {
-                        return <div key={index} product={product} className="products">
-                            <img src={`http://localhost:4000/uploads/${product.imagePath}`} alt="product"></img>
-                
+                        return <div key={index} className="product">
+                            <img src={`http://localhost:4000/uploads/${product.imagePath}`} alt="product" />
+                            <div className="product-details-container">
                                 <p className="products-title">{product.title}</p>
-                                <div className="products-details">
                                 <p className="products-description">{product.description}</p>
-                                <p className="products-price">{product.price} €</p>
-                                <button className="order-button" onClick={() => addToCart(product)}>Order</button>
+                                <div className="products-details">
+                                    <p className="products-price">{product.price}€</p>
+                                    <button className="order-button" onClick={() => addToCart(product)}><AiOutlinePlus className="add-icon" />
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     })}
                 </div>
+                <div className="main-products-overview-pagination">
+                    {totalPages > 1 && <Pagination currentPage={currentPage} handlePageChange={handlePageChange} totalPages={totalPages} />}
+                </div>
             </div>
-            <Cart isCartOpen={isCartOpen} toggleCart={toggleCart} emptyCart={emptyCart}  clearCartProduct={clearCartProduct} cart={cart} />
+            <Cart isCartOpen={isCartOpen} toggleCart={toggleCart} cart={cart} clearCartProduct={clearCartProduct} emptyCart={emptyCart} />
         </div>
     );
 }
