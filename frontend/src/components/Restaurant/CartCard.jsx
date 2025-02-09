d574c9ce473e3711c41ffaf4161c4283d634c859
TauraNam <tauranamirskaite@gmail.com>
2025-02-09 16:22:34 +0200

    CartCard component changes

--- frontend/src/components/Restaurant/CartCard.jsx
+++ frontend/src/components/Restaurant/CartCard.jsx
@@ -1,9 +1,10 @@
-const CartCard = ({button, item}) => {
+const CartCard = ({ button, item, className }) => {
+
     return (
-        <div className="cart-content-details">
-            <img src={`http://localhost:4000/uploads/${item.imagePath}`} alt="product"></img>
+        <div className={`cart-content-details ${className}`}>
+            <img src={`http://localhost:4000/uploads/${item.imagePath}`} alt="product" />
             <p className="cart-title">{item.quantity > 1 ? `${item.quantity} x` : ''} {item.title}</p>
-            <p className="cart-price">{item.price} €</p>
+            <p className="cart-price">{item.price}€</p>
             {button && button}
         </div>
     );
